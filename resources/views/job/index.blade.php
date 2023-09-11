c27a98264fbb7cd8ba8f7faad172f895998e862e
KotKHPI <prototype2428@gmail.com>
2023-09-11 13:39:06 +0300

    Make styling

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -1,5 +1,7 @@
 <x-layout>
     @foreach($jobs as $job)
-        <div>{{ $job->title }}</div>
+        <div class="rounded-md border border-slate-300 bg-white p-4 shadow-sm mb-4">
+            {{ $job->title }}
+        </div>
     @endforeach
 </x-layout>
