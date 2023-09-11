69198eb969cc3778c540436647f509014bae3ca5
KotKHPI <prototype2428@gmail.com>
2023-09-11 13:51:02 +0300

    Add card layout

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
     @foreach($jobs as $job)
-        <div class="rounded-md border border-slate-300 bg-white p-4 shadow-sm mb-4">
+        <x-card class="mb-4">
             {{ $job->title }}
-        </div>
+        </x-card>
     @endforeach
 </x-layout>
