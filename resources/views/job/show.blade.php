f7881139a2ed56f1c3e5717f7bc8af5068b58ce8
KotKHPI <prototype2428@gmail.com>
2023-09-12 11:13:54 +0300

    Make navigate the site

--- resources/views/job/show.blade.php
+++ resources/views/job/show.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-    <x-job-card :$job>
-
-    </x-job-card>
+    <x-breadcrumbs class="mb-4"
+    :links="['Jobs' => route('jobs.index'), $job->title => '#' ]" />
+    <x-job-card :$job />
 </x-layout>
