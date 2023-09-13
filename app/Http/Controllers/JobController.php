3bd0cc6bb2bc52ef99368aa2705d2e05d7a59884
KotKHPI <prototype2428@gmail.com>
2023-09-13 12:43:14 +0300

    Make component for radio group and add category filter

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -27,6 +27,9 @@ class JobController extends Controller
                 })
                 ->when(request('experience'), function ($query) {
                     $query->where('experience', request('experience'));
+                })
+                ->when(request('category'), function ($query) {
+                    $query->where('category', request('category'));
                 });
         });
 
