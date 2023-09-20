d53cf0b28f67d99e6992ef0fce3e17446c84d05d
KotKHPI <prototype2428@gmail.com>
2023-09-20 18:19:00 +0300

    Make form apply job

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\JobApplicationController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
 
@@ -24,4 +25,8 @@ Route::resource('auth', AuthController::class);
 
 Route::delete('logout', fn() => to_route('auth.destroy'))->name('logout');
 Route::delete('auth', [AuthController::class, 'destroy'])->name('auth.destroy');
+
+Route::middleware('auth')->group(function () {
+    Route::resource('job.application', JobApplicationController::class)->only(['store', 'create']);
+});
 
