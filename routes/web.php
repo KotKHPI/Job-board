3f62a7d9176d01a1e7ca690e347c0c08a882e803
KotKHPI <prototype2428@gmail.com>
2023-09-20 15:58:58 +0300

    Add sing-in logic

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
 
@@ -17,3 +18,6 @@ use App\Http\Controllers\JobController;
 Route::get('/', fn() => to_route('jobs.index'));
 
 Route::resource('jobs', JobController::class);
+
+Route::get('login', fn() => to_route('auth.create'))->name('login');
+Route::resource('auth', AuthController::class);
