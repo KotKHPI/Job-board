e98894b2964ff758ae7a3d58b0126e88f1fc5e3f
KotKHPI <prototype2428@gmail.com>
2023-09-21 12:51:49 +0300

    Added cancel applied jobs form and logic

--- app/Http/Controllers/MyJobApplicationController.php
+++ app/Http/Controllers/MyJobApplicationController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\JobApplication;
 use Illuminate\Http\Request;
 
 class MyJobApplicationController extends Controller
@@ -9,12 +10,20 @@ class MyJobApplicationController extends Controller
     public function index()
     {
         return view('my_job_application.index', [
-            'applications' => auth()->user()->jobApplications()->with('job.employer')->latest()->get()
+            'applications' => auth()->user()->jobApplications()
+                ->with([
+                    'job' => fn($query) => $query->withCount('jobApplications')
+                    ->withAvg('jobApplications', 'expected_salary'),
+                    'job.employer'
+                ])
+                ->latest()->get()
         ]);
     }
 
-    public function destroy(string $id)
+    public function destroy(JobApplication $myJobApplication)
     {
-        //
+        $myJobApplication->delete();
+
+        return redirect()->back()->with('success', 'The job application was removed.');
     }
 }
