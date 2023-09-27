4ec1686080802d5bb9485b3113c9380fd2221052
KotKHPI <prototype2428@gmail.com>
2023-09-27 15:24:42 +0300

    Make employer's form for adding a new job

--- resources/views/components/radio-group.blade.php
+++ resources/views/components/radio-group.blade.php
@@ -1,15 +1,17 @@
 <div>
-    <label for="{{$name}}" class="mb-1 flex items-center">
-        <input type="radio" name="{{$name}}" value=""
-            @checked(!request($name))/>
-        <span class="ml-2">All</span>
-    </label>
+    @if($allOption)
+        <label for="{{$name}}" class="mb-1 flex items-center">
+            <input type="radio" name="{{$name}}" value=""
+                @checked(!request($name))/>
+            <span class="ml-2">All</span>
+        </label>
+    @endif
 
     @foreach($optionsWithLabels as $lable => $option)
 
     <label for="{{$name}}" class="mb-1 flex items-center">
         <input type="radio" name="{{$name}}" value="{{$option}}"
-            @checked($option === request($name))/>
+            @checked($option === ($value ?? request($name)))/>
         <span class="ml-2">{{$lable}}</span>
     </label>
 
